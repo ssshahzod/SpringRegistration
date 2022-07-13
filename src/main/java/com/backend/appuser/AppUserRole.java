85520d05ca7544ea085767a2c31e5e3c01fb1d62
Cronion <ohmytarik@mail.ru>
2022-07-13 18:39:44 +0300

    .

---
+++ src/main/java/com/backend/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.backend.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN,
+}
---
+++ src/main/java/com/backend/registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.backend.registration;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
